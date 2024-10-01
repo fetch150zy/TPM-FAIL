cb5d1394c1ce6a2ad26d558c63258252dd1bead4
fetch150zy <mars_zhewei@outlook.com>
2024-10-01 14:31:46 +0800

    update

--- x86/workspace/setup-kernel.sh
+++ x86/workspace/setup-kernel.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-sudo insmod ../kernel/tpmttl.ko
+sudo insmod ../kernel/tpmttlcrb.ko
+sudo insmod ../kernel/tpmttltis.ko
 sudo ../client/tpmttl 2
 sudo ../client/tpmttl 3 >> /dev/null
 
